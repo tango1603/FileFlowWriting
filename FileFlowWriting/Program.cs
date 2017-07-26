825d5985ca1c45623692479b6b33d4af9d8d6f97
Илья Башилов <ilya.bashilov@simbirsoft.com>
2017-07-26 13:34:43 +0300

    Исправил оформление кода. Удалил ненужные коменты

--- FileFlowWriting/Program.cs
+++ FileFlowWriting/Program.cs
@@ -46,8 +46,7 @@ namespace FileFlowWriting
             // создаем новый поток
             Thread myThread = new Thread(Count);
             myThread.Start(); // запускаем поток
-
-            
+                        
             for (int i = 1; i < 20; i++)
             {
                 waitHandler.WaitOne();
@@ -59,9 +58,7 @@ namespace FileFlowWriting
                 }
                 Thread.Sleep(200);
                 waitHandler.Set();
-            }
-            
-
+            }            
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
@@ -69,103 +66,19 @@ namespace FileFlowWriting
         public static void Count()
         {
             
-                for (int i = 1; i < 20; i++)
-                {
+            for (int i = 1; i < 20; i++)
+            {
                 waitHandler.WaitOne();
                 Console.ForegroundColor = ConsoleColor.Red;     // Устанавливаем красный цвет символов    
-                    Console.WriteLine("Пишет в файл ВТОРОЙ поток:");
-                    using (StreamWriter sw = new StreamWriter((string)fullFolderFileName, true, Encoding.Default))
-                    {
-                        sw.WriteLine("Этот текст создан во ВТОРОМ потоке");
-                    }
-                    Console.ResetColor(); // Устанавливаем белый цвет символов
-                    Thread.Sleep(300);
+                Console.WriteLine("Пишет в файл ВТОРОЙ поток:");
+                using (StreamWriter sw = new StreamWriter((string)fullFolderFileName, true, Encoding.Default))
+                {
+                     sw.WriteLine("Этот текст создан во ВТОРОМ потоке");
+                }
+                Console.ResetColor(); // Устанавливаем белый цвет символов
+                Thread.Sleep(300);
                 waitHandler.Set();
-            }
-                
-            
-        }
-
-
-
-        /*
-            try
-            {
-                Console.WriteLine("******считываем весь файл********");
-                using (StreamReader sr = new StreamReader(fullFolderFileName))
-                {
-                    Console.WriteLine(sr.ReadToEnd());
-                }
-
-                Console.WriteLine();
-                Console.WriteLine("******считываем построчно********");
-                using (StreamReader sr = new StreamReader(fullFolderFileName, Encoding.Default))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(line);
-                    }
-                }
-
-                Console.WriteLine();
-                Console.WriteLine("******считываем блоками********");
-                using (StreamReader sr = new StreamReader(fullFolderFileName, Encoding.Default))
-                {
-                    char[] array = new char[4];
-                    // считываем 4 символа
-                    sr.Read(array, 0, 4);
-                    Console.WriteLine(array);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }*/
-
-        //******************************************работа с потоком*********************************************
-
-        /*  Thread t = Thread.CurrentThread;
-            t.Name = "Метод Main";
-            Console.WriteLine("Имя потока: {0}", t.Name);
-            Console.WriteLine("Запущен ли поток: {0}", t.IsAlive);
-            Console.WriteLine("Приоритет потока: {0}", t.Priority);
-            Console.WriteLine("Статус потока: {0}", t.ThreadState);
-            // получаем домен приложения
-            Console.WriteLine("Домен приложения: {0}", Thread.GetDomain().FriendlyName);
-
-    }    
-
-       /* class Program
-        {
-            static int x = 0;
-            static object locker = new object();
-            static void Main(string[] args)
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    Thread myThread = new Thread(Count);
-                    myThread.Name = "Поток " + i.ToString();
-                    myThread.Start();
-                }
-
-                Console.ReadLine();
-            }
-            public static void Count()
-            {
-                lock (locker)
-                {
-                    x = 1;
-                    for (int i = 1; i < 9; i++)
-                    {
-                        Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
-                        x++;
-                        Thread.Sleep(100);
-                    }
-                }
-            }
-        }
-        */
-
+            }                 
+        }      
     }
 }
